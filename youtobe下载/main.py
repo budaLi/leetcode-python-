acbd91f5f34272d30d62c36ea4d1ff24da38e928
lenovo <1364826576@qq.com>
2019-12-17 10:49:27 +0800

    youtobe视频下载

---
+++ youtobe下载/main.py
@@ -0,0 +1,17 @@
+# @Time    : 2019/12/17 9:24
+# @Author  : Libuda
+# @FileName: main.py
+# @Software: PyCharm
+import subprocess
+
+
+# 下载视频函数
+def down_videos(url):
+    urls = r'C:\Users\lenovo\Desktop\开发\youtube-dl.exe {} % '.format(url)
+    p = subprocess.Popen(urls, shell=True, universal_newlines=True)
+    p.wait()  # 阻塞，等待子进程完成
+    print('------>', p.returncode)  # 判断执行状态，成功返回0
+
+
+if __name__ == '__main__':
+    down_videos("https://www.youtube.com/watch?v=4UP-p3ZP5cA")
