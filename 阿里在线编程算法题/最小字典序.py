6061cd3cfd7ceb132b262ae0cadf3465b1d565fd
lenovo <1364826576@qq.com>
2020-01-16 18:48:00 +0800

    2020-1-16未完成

---
+++ 阿里在线编程算法题/最小字典序.py
@@ -0,0 +1,52 @@
+# @Time    : 2020/1/16 18:13
+# @Author  : Libuda
+# @FileName: 最小字典序.py
+# @Software: PyCharm
+
+def Solution(data):
+    """
+    输入：
+    "AABACBBC"
+    输出：
+    "AAABBBCC"
+    :param m:
+    :return:
+    """
+    index = 0
+    lenght = len(data) - 1
+    data = list(data)
+
+    while index < lenght:
+        print(data)
+        if data[index] > data[index + 1]:
+            if ord(data[index]) - ord(data[index + 1]) == 1:
+                "ABABAB—> AABBAB "
+                "当位置1和位置2交换后 需要从2开始找最近一个比他小的A交换"
+                data[index], data[index + 1] = data[index + 1], data[index]
+                tem = index + 1  # 记录当前进行到哪一个字符 "B"
+                while index < lenght:
+                    if data[index] == "A":
+                        data[tem + 1] = "B"
+                        data[index] = data[tem]
+                        break
+                    index += 1
+
+
+
+            else:
+                "CAAAAB 这种情况 需要找到最近的一个B进行交换 变成BCAAAA这种"
+                tem = index  # 记录当前进行到哪一个字符 "C"
+                while index < lenght:
+                    index += 1
+                    if data[index] == "B":
+                        data[tem + 1] = data[tem]
+                        data[tem] = data[index]
+                        data[tem + 2:index + 1] = "A" * (index - tem - 2 + 1)
+        index += 1
+
+    return "".join(data)
+
+
+tem = "ABABAB"
+res = Solution(tem)
+print(res)
