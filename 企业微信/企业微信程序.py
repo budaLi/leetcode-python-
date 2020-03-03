bbc0ab35186dfe3316c1d6fc601a91212c57f49b
lenovo <1364826576@qq.com>
2020-03-03 23:39:26 +0800

    企业微信发送消息

--- 企业微信/企业微信程序.py
+++ 企业微信/企业微信程序.py
@@ -4,14 +4,18 @@
 import time
 import requests
 import json
+from datetime import datetime, time as dtime
+from 企业微信.邮件接收程序 import get_rev, logger
+from 企业微信.config import get_config
 
+config = get_config()
 
 class WeChat:
     def __init__(self):
-        self.CORPID = 'wwefe26840c41c9ef8'  # 企业ID，在管理后台获取
-        self.CORPSECRET = 'sj4WFEc-R5Wlw8CrTzxxlSKMfZB4vXTos9nS796y-ZQ'  # 自建应用的Secret，每个自建应用里都有单独的secret
-        self.AGENTID = '1000002'  # 应用ID，在后台应用中获取
-        self.TOUSER = "@all"  # 接收者用户名,多个用户用|分割
+        self.CORPID = config['CORPID']
+        self.CORPSECRET = config['CORPSECRET']
+        self.AGENTID = config['AGENTID']
+        self.TOUSER = config['TOUSER']
 
     def _get_access_token(self):
         url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
@@ -58,7 +62,31 @@ class WeChat:
         respone = respone.json()  # 当返回的数据是json串的时候直接用.json即可将respone转换成字典
         return respone["errmsg"]
 
-
 if __name__ == '__main__':
     wx = WeChat()
-    wx.send_data("以上为测试信息……\n正式信号推送敬请期待 ")
+
+    # 程序运行时间在白天8:55 到 11:30  下午1:30 到  3:01
+    DAY_START = dtime(8, 55)
+    DAY_END = dtime(11, 30)
+
+    NIGHT_START = dtime(13, 30)
+    NIGHT_END = dtime(23, 59)
+
+    # 接收邮件
+    rev = get_rev()
+
+    while 1:
+        current_time = datetime.now().time()
+        if DAY_START <= current_time <= DAY_END or (NIGHT_START <= current_time <= NIGHT_END):
+            # 判断时候在可运行时间内
+            logger("等待接收邮件中")
+            try:
+                content = rev.run_ing()
+                if content:
+                    print("接收到邮件：{}".format(content))
+                    wx.send_data(content)
+            except Exception as e:
+                print("运行错误", e)
+        else:
+            time.sleep(2)
+            logger("其他时间段  其他任务执行中")
--- 企业微信/邮件接收程序.py
+++ 企业微信/邮件接收程序.py
@@ -4,13 +4,15 @@
 # @Software: PyCharm
 # -*- coding: utf-8 -*-
 
-import poplib, email, telnetlib
-import datetime, time, sys, traceback
+import poplib
+import time
 from email.parser import Parser
 from email.header import decode_header
 from email.utils import parseaddr
-import imaplib
+from 企业微信.config import get_config
+import re
 
+config = get_config()
 
 def logger(msg):
     """
@@ -20,14 +22,22 @@ def logger(msg):
     print("[%s] %s" % (now, msg))
 
 
-class down_email():
+class ReciveEmail():
 
-    def __init__(self, user, password, eamil_server):
+    def __init__(self):
+
+        # 收件人
+        self.user = config['rev_user']
+
+        # 指定发件人
+        self.users = config['send_users'].split(",")
+
         # 输入邮件地址, 口令和POP3服务器地址:
-        self.user = user
+        self.email_server = config['email_server']
+
         # 此处密码是授权码,用于登录第三方邮件客户端
-        self.password = password
-        self.pop3_server = eamil_server
+        self.password = config['password']
+
         self.count = 0
         self.totle_set = None  # 邮件去重
 
@@ -76,13 +86,13 @@ class down_email():
                 result.append(content)
         return ",".join(result)
 
-    def run_ing(self, users, second):
+    def run_ing(self):
         try:
             # telnetlib.Telnet(self.pop3_server, 995)
-            self.server = poplib.POP3(self.pop3_server, 110, timeout=10)
-        except:
-
-            self.server = poplib.POP3(self.pop3_server, 110, timeout=10)
+            self.server = poplib.POP3(self.email_server, 110, timeout=10)
+        except Exception as e:
+            print("服务器连接失败", e)
+            self.server = poplib.POP3(self.email_server, 110, timeout=10)
 
         # 打印POP3服务器的欢迎文字:
         # print("身份验证成功，下面为测试输出：{}".format(self.server.getwelcome().decode('utf-8')))
@@ -90,7 +100,7 @@ class down_email():
         self.server.user(self.user)
         self.server.pass_(self.password)
         # 返回邮件数量和占用空间:
-        logger('邮件总数: %s. 大小: %s' % self.server.stat())
+        # logger('邮件总数: %s. 大小: %s' % self.server.stat())
 
         # list()返回所有邮件的编号:
         resp, mails, octets = self.server.list()
@@ -130,16 +140,17 @@ class down_email():
                 # 邮件时间格式转换
                 # date2 = time.strftime("%Y-%m-%d %H:%M:%S", date1)
 
-                if From in users:
+                if From in self.users:
                     # print(date2,start_time)
-                    print("\n")
-                    print('发件人:%s,收件人:%s,抄送人:%s,主题:%s' % (From, To, Cc, Subject))
+                    # print("\n")
+                    # print('发件人:%s,收件人:%s,抄送人:%s,主题:%s' % (From, To, Cc, Subject))
                     content = self.get_att(msg)
-                    # content = msg
+                    # print("邮件正文:{} \n".format(content))
+                    return content
 
-                    print("邮件正文:{} \n".format(content))
             except Exception as e:
                 print(e, "解析错误")
+                return None
 
         # 可以根据邮件索引号直接从服务器删除邮件:
         # self.server.dele(7)
@@ -148,28 +159,30 @@ class down_email():
         self.server.quit()
 
 
+def trans_content(coontent):
+    # res_tem =
+    content = "模组名: 沪铅主连_1分钟_金肯特纳(1)" \
+              "模型名: 金肯特纳合约名: 沪铅主连   " \
+              "周期: 1分钟14:57:58 [运行日志]14:57:58(本机时间): " \
+              "信号30 平仓盈亏 -300 <合计 -1000>"
+
+    # partten = re.compile(r"模组名.* $")
+    # res = re.findall(partten,content)
+    # print(res)
+
+
+def get_rev():
+    reciveemail = ReciveEmail()
+    return reciveemail
+
+
+
 if __name__ == '__main__':
-
-    # 收件人邮箱及密码
-    user = 'admin@toujizhe.com.cn'
-    password = 'A1234567'
-
-    # 指定发件人
-    users = ['WebStockWh8@wenhua.com.cn', '3405987953@qq.com', '1364826576@qq.com', '1410000000@qq.com']
-    # 刷新时间间隔
-    time_s = 1
-
-    eamil_server = 'pop3.toujizhe.com.cn'
-    email_class = down_email(user=user, password=password, eamil_server=eamil_server)
-    # #
-    # s =b'\\u6a21\\u7ec4\\u540d: \\u6ca5\\u97522006_1\\u5206\\u949f_\\u91d1\\u80af\\u7279\\u7eb3(1)\r\n\\u6a21\\u578b\\u540d: \\u91d1\\u80af\\u7279\\u7eb3\r\n\\u5408\\u7ea6\\u540d: \\u6ca5\\u97522006   \\u5468\\u671f: 1\\u5206\\u949f\r\n13:34:58 [\\u8fd0\\u884c\\u65e5\\u5fd7]13:34:58(\\u672c\\u673a\\u65f6\\u95f4  ): \\u4fe1\\u53f78 '
-    # print(s.decode("unicode-escape"))
-    # print(s2)
-    # print(type(s2),s2.encode("utf-8").decode("gb2312"))
-    # print("\u6a21\u7ec4\u540d: PTA\u4e3b\u8fde_1\u5206\u949f_\u91d1\u80af\u7279\u7eb3(1)\u6a21\u578b\u540d:".encode("utf-8"))
-    while 1:
-        try:
-            email_class.run_ing(users, time_s)
-            time.sleep(time_s)
-        except Exception as e:
-            print(e)
+    trans_content(1)
+    # reciveemail= get_rev()
+    # while 1:
+    #     try:
+    #         content = reciveemail.run_ing()
+    #         print(content)
+    #     except Exception as e:
+    #         print("运行错误",e)
