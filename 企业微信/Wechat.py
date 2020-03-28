101b959785500c02ef67a99ed0c8d160a0e191f7
lenovo <1364826576@qq.com>
2020-03-28 13:42:05 +0800

    发送视频 音频 图片

--- 企业微信/Wechat.py
+++ 企业微信/Wechat.py
@@ -6,8 +6,6 @@
 import time
 import requests
 import json
-import base64
-import hashlib
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 from 企业微信.config import get_config
 
@@ -68,25 +66,28 @@ class WeChat:
                     f.write('\t'.join([str(cur_time), access_token]))
                     return access_token
 
-    def upload_image(self, file_path):
+    def upload_media(self, file_path, file_type):
         """
-        上传图片临时素材  multipart/form-data 图片上传
+        上传临时素材  multipart/form-data 上传
         参考：https://blog.csdn.net/Magician_vv/article/details/90478019?depth_1-utm_source=distribute.pc_relevant.none-task&utm_source=distribute.pc_relevant.none-task
         :param file_path: 文件路径
         :param file_type: 文件类型
         :return:图片media_id
         """
-        request_url = self.upload_url.format(self.get_access_token(), "image")
+        request_url = self.upload_url.format(self.get_access_token(), file_type)
         headers = {}
         multipart_encoder = MultipartEncoder(
             fields={
                 # 这里根据服务器需要的参数格式进行修改
-                'file': (file_path, open(file_path, 'rb'), 'image/jpeg'),
+                'file': (file_path, open(file_path, 'rb'), file_type),
             },
         )
         headers['Content-Type'] = multipart_encoder.content_type
         response = requests.post(request_url, data=multipart_encoder, headers=headers).json()
-        return response['media_id']
+        if response['errmsg'] == "ok":
+            return response['media_id']
+        return response['errmsg']
+
 
     def send_data(self, message):
         send_url = self.send_url.format(self.get_access_token())
@@ -104,25 +105,28 @@ class WeChat:
         respone = respone.json()  # 当返回的数据是json串的时候直接用.json即可将respone转换成字典
         return respone["errmsg"]
 
-    def send_image(self, media_id, touser):
+    def send_media_data(self, media_id, msgtype, touser):
         """
-        发送图片
+        发送图片.音频，视频文件
+        分别有图片（image）、语音（voice）、视频（video），普通文件（file）
         :param media_id: 图片media_id  可通过upload_image()获取
         :return:
         """
         send_url = self.send_url.format(self.get_access_token())
         send_values = {
             "touser": touser,
-            "msgtype": "image",
+            "msgtype": msgtype,
             "agentid": self.AGENTID,
-            "image": {
+            msgtype: {
                 "media_id": media_id
             },
         }
-        send_msges = (json.dumps(send_values))
-        respone = requests.post(send_url, send_msges)
-        respone = respone.json()  # 当返回的数据是json串的时候直接用.json即可将respone转换成字典
-        return respone["errmsg"]
+        send_msges = json.dumps(send_values)
+        response = requests.post(send_url, send_msges).json()
+
+        if response['errmsg'] == "ok":
+            return response
+        return response['errmsg']
 
     def get_user_id_by_phone(self, phone):
         """
@@ -138,7 +142,7 @@ class WeChat:
         }
         response = requests.post(request_url, data=json.dumps(request_data)).json()
         if response['errmsg'] == "ok":
-            return response
+            return response['userid']
         return response['errmsg']
 
     def create_menu(self):
@@ -148,7 +152,7 @@ class WeChat:
         """
         request_url = "https://qyapi.weixin.qq.com/cgi-bin/menu/create?access_token={}&agentid={}".format(
             self.get_access_token(), self.AGENTID)
-        request_data = {
+        request_data1 = {
             "button": [
                 {
                     "name": "扫码",
@@ -197,9 +201,32 @@ class WeChat:
                 }
             ]
         }
-
+        request_data2 = {
+            "button": [
+                {
+                    "type": "click",
+                    "name": "今日歌曲",
+                    "key": "V1001_TODAY_MUSIC"
+                },
+                {
+                    "name": "菜单",
+                    "sub_button": [
+                        {
+                            "type": "view",
+                            "name": "搜索",
+                            "url": "http://www.soso.com/"
+                        },
+                        {
+                            "type": "click",
+                            "name": "赞一下我们",
+                            "key": "V1001_GOOD"
+                        }
+                    ]
+                }
+            ]
+        }
         # 传入的data需要是json格式
-        response = requests.post(request_url, data=json.dumps(request_data)).json()
+        response = requests.post(request_url, data=json.dumps(request_data2)).json()
         if response['errmsg'] == "ok":
             return response
         return response['errmsg']
@@ -207,8 +234,12 @@ class WeChat:
 
 if __name__ == '__main__':
     wx = WeChat()
-    res = wx.get_user_id_by_phone("15735656005")
+    # res = wx.create_menu()
+    userid = wx.get_user_id_by_phone("15735656005")
+    print(userid)
+    #
+    media = wx.upload_media("3.mp4", 'video')
+    print(media)
 
-    media = wx.upload_image("1.jpg")
-    res2 = wx.send_image(media, "BuDa")
+    res2 = wx.send_media_data(media, "video", userid)
     print(res2)
