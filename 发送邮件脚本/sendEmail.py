75bb0b56518e2e7f8b5fdd6cdab34cdb1036068c
lenovo <1364826576@qq.com>
2020-07-10 11:26:42 +0800

    邮箱发送文本、图片、视频等文件

--- 发送邮件脚本/sendEmail.py
+++ 发送邮件脚本/sendEmail.py
@@ -1,6 +1,8 @@
 import smtplib  # 发送邮件 连接邮件服务器
 from email.mime.text import MIMEText  # 构建邮件格式
-
+from email.mime.multipart import MIMEMultipart  # 发送多个部分
+from email.mime.image import MIMEImage  # 图片格式
+from email.mime.application import MIMEApplication  # 发送附件
 
 class SendEmail:
     def __init__(self):
@@ -13,11 +15,7 @@ class SendEmail:
         # 发送邮件的服务器地址 qq为smtp.qq.com  163邮箱为smtp.163.com
         self.email_host = 'smtp.qq.com'
 
-    def send_email(self, userlist, subject, content):
-        message = MIMEText(content, _subtype='plain', _charset='utf-8')
-        message['Subject'] = subject
-        message['From'] = self.send_user
-        message['To'] = ';'.join(userlist)  # 收件人列表以分号隔开
+    def _send(self, userlist, message):
         # 实例化邮件发送服务器
         server = smtplib.SMTP()
         # 连接qq邮箱服务器
@@ -29,28 +27,64 @@ class SendEmail:
         # 关闭邮箱
         server.close()
 
-    def send_test(self, userlist, passNumber, failNumber):
+    def send_text(self, userlist, subject, content):
         """
-        发送测试结果
-        :param userlist:
-        :param passNumber:
-        :param failNumber:
+        发送文本邮件
+        :param userlist: 接收人  列表形式
+        :param subject: 主题
+        :param content:  内容
         :return:
         """
-        totalNumber = passNumber + failNumber
+        message = MIMEText(content, _subtype='plain', _charset='utf-8')
+        message['Subject'] = subject
+        message['From'] = self.send_user
+        message['To'] = ';'.join(userlist)  # 收件人列表以分号隔开
+        self._send(userlist, message)
 
-        # %.2f表示保留小数点后两位小数 %%表示百分之百
-        passPercentage = "%.2f%%" % (passNumber / totalNumber * 100)
-        failPercentae = "%.2f%%" % (failNumber / totalNumber * 100)
+    def send_image(self, userlist, subject, img_path):
+        """
+        发送图片
+        :param userlist:收件人列表
+        :param subject:主题
+        :param img_path:图片路径
+        :return:
+        """
+        message = MIMEMultipart('related')
+        content = MIMEText('<html><body><img src="cid:imageid" alt="imageid"></body></html>', 'html', 'utf-8')  # 正文
+        message.attach(content)
+        message['Subject'] = subject
+        message['From'] = self.send_user
+        message['To'] = ';'.join(userlist)  # 收件人列表以分号隔开
 
-        sub = "测试结果报告"
-        content = " 测试用例总数\t%s个\n通过个数\t%s个\n失败个数\t%s个\n通过率\t%s\n失败率\t%s" % (
-            totalNumber, passNumber, failNumber, passPercentage, failPercentae)
-        self.send_email(userlist, sub, content)
-        return True
+        file = open(img_path, "rb")
+        img_data = file.read()
+        file.close()
 
+        img = MIMEImage(img_data)
+        img.add_header('Content-ID', 'imageid')
+        message.attach(img)
+
+        self._send(userlist, message)
+
+    def send_vedio(self, userlist, subject, file):
+        """
+        发送文件
+        """
+        message = MIMEMultipart('related')
+        content = MIMEText('<html><body><img src="cid:imageid" alt="imageid"></body></html>', 'html', 'utf-8')  # 正文
+        message.attach(content)
+        message['Subject'] = subject
+        message['From'] = self.send_user
+        message['To'] = ';'.join(userlist)  # 收件人列表以分号隔开
+
+        part_attach1 = MIMEApplication(open(file, 'rb').read())  # 打开附件
+        part_attach1.add_header('Content-Disposition', 'attachment', filename=file)  # 为附件命名
+        message.attach(part_attach1)  # 添加附件
+
+        self._send(userlist, message)
 
 if __name__ == "__main__":
     send = SendEmail()
     user_list = ['1364826576@qq.com']
-    send.send_test(user_list, 4, 5)
+    send.send_vedio(user_list, "图片", "1.jpeg")
+    # send.send_text(user_list, 4, 5)
